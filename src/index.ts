790c2185a3b4b2249aca7db3286221d3ade5a5ab
Nate Stemo <natestemo@hotmail.com>
2024-05-09 13:18:57 -0500

    implement function in index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import {getRandomAthleteScreenshot} from "./screenshotRandomAthlete";
 import { getRandomMeetScreenshot } from "./screenshotRandomMeet";
-import { sendMail } from "./sendMail";
+import {sendMail}  from "./sendMail";
 
 async function run() {
     // https://liftoracle.com/api/v1/athletes?pageSize=12000
@@ -9,6 +9,7 @@ async function run() {
         console.log(randomAthlete);
         const randomMeet = await getRandomMeetScreenshot();
         console.log(randomMeet);
+        await sendMail(randomMeet, randomAthlete )
         
     }catch(error){
         console.error(error);
