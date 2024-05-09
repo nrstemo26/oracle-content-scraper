b3dbc0ed6f56d972d35184cde7200f3c2b4f479d
Nate Stemo <natestemo@hotmail.com>
2024-05-09 12:38:52 -0500

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-// import { chromium } from 'playwright';
 import {getRandomAthleteScreenshot} from "./screenshotRandomAthlete";
-
-
-
 
 async function run() {
     // https://liftoracle.com/api/v1/athletes?pageSize=12000
@@ -13,13 +9,6 @@ async function run() {
     }catch(error){
         console.error(error);
     }
-
-
-    // const browser = await chromium.launch();
-    // const page = await browser.newPage();
-    // await page.goto('https://example.com');
-    // await page.screenshot({ path: 'example.png' });
-    // await browser.close();
 }
 
 run().catch(console.error);
