84d3b1bae2401c3ce705e92896e23bb7ee697e08
Nate Stemo <natestemo@hotmail.com>
2024-05-13 10:34:43 -0500

    gmail oauth set up for nodemailer

--- src/sendMail.ts
+++ src/sendMail.ts
@@ -1,28 +1,32 @@
 import { config } from 'dotenv';
 config();
-import * as nodemailer from 'nodemailer';
+const nodemailer = require('nodemailer');
 
 export async function sendMail(meetScreenshotPath: string, athleteScreenshotPath: string) {
     try{
         const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
+                type: 'OAuth2',
                 user: process.env.EMAIL_USERNAME,
-                pass: process.env.EMAIL_PASSWORD
+                pass: process.env.EMAIL_PASSWORD,
+                clientId: process.env.CLIENT_ID,
+                clientSecret: process.env.CLIENT_SECRET,
+                refreshToken: process.env.REFRESH_TOKEN,
             }
         });
         const mailOptions = {
             from: process.env.EMAIL_USERNAME,
-            to: 'nate@milwaukeebarbell.com',
+            to: 'murph@milwaukeebarbell.com',
             subject: 'Lift Oracle Screenshots',
-            text: 'Here are the screenshots',
+            text: 'Here are the random screenshots',
             attachments: [
                 { path: meetScreenshotPath },
                 { path: athleteScreenshotPath }
             ]
         }
 
-        transporter.sendMail(mailOptions, function(error, info){
+        transporter.sendMail(mailOptions, function(error:any, info: any){
             if (error) {
                 console.log(error);
             } else {
